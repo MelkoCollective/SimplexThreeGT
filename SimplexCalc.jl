eda497b582e6d9b782fef6c963416c1921341e72
Roger Melko <rgmelko@uwaterloo.ca>
2022-05-21 11:37:30 -0400

    basic d=3 indexing

--- SimplexCalc.jl
+++ SimplexCalc.jl
@@ -2,15 +2,43 @@
 
 function Cube_Label(Dim,L)
 
-    Nspin = 3*L^3
+    Ncube = L^3
+    Nspin = 3*Ncube
 
-    Cube = zeros(Int,Nspin,Dim)
+    Cube = zeros(Int,Ncube,6)
 
+    # First round 
     for i = 1:L^3
-        Cube[i,1] = i
-        Cube[i,2] = i+1
-        Cube[i,3] = i+2
+        Cube[i,1] = Dim*(i-1) + 1
+        Cube[i,2] = Dim*(i-1) + 2
+        Cube[i,3] = Dim*(i-1) + 3
     end
+
+    # Second round 
+    i = 0
+    for z=1:L
+        for y=1:L
+            for x=1:L
+                i += 1
+                if (x==L) #X-NEIGHBOR
+                    Cube[i,4] = Cube[i-L+1,3]
+                else
+                    Cube[i,4] = Cube[i+1,3]
+                end
+                if (y==L) #Y-NEIGHBOR
+                    Cube[i,5] =  Cube[i-L^2+L,1]
+                else
+                    Cube[i,5] =  Cube[i+L,1]
+                end
+                if (z==L) #Z-NEIGHBOR
+                    Cube[i,6] = Cube[i-L^3+L^2,2]
+                else
+                    Cube[i,6] = Cube[i+L^2,2]
+                end
+             end #x
+        end #y
+    end #z
+
 
    println(Cube)
 
@@ -19,7 +47,7 @@ end
 #-----------------------MAIN---------------------
 
 Dim = 3
-L = 2 
+L = 3 
 
 Cube_Label(Dim,L) 
 
