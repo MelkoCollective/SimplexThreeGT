7b5961f57b6c8a69337b8fd31c44feeb72eb3f36
Roger-luo <rogerluo.rl18@gmail.com>
2022-12-07 21:58:12 -0500

    make slurm job

--- scripts/task.sh
+++ scripts/task.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 #SBATCH --account=kfm-471-aa
 #SBATCH --time=48:00:00
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=1
+#SBATCH --array=4-16:2
 module load julia/1.8.1
-julia --threads=auto --project scripts/main.jl 4 4  --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 6  --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 8  --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 10 --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 12 --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 14 --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 16 --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 18 --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
-julia --threads=auto --project scripts/main.jl 4 20 --nsamples=1000000 --nburns=10000 --seed=1234 --nthrows=50
+julia --project scripts/main.jl annealing --task=task/3d${SLURM_ARRAY_JOB_ID}L.toml
