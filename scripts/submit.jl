ad9c696d6a68f7ed2afbee06987c760b921ca377
Roger-luo <rogerluo.rl18@gmail.com>
2022-12-07 23:57:56 -0500

    update scripts

---
+++ scripts/submit.jl
@@ -0,0 +1,54 @@
+using Comonicon
+
+function template(type, d, L, nthreads::Int, mem::Int)
+    """#!/bin/bash
+    #SBATCH --account=rrg-rgmelko-ab
+    #SBATCH --time=48:00:00
+    #SBATCH --cpus-per-task=$nthreads
+    #SBATCH --mem=$(mem)G
+    #SBATCH --job-name=$(type)_$(d)d$(L)L
+    #SBATCH -o logs/%j.out
+    #SBATCH -e logs/%j.err
+    module load julia/1.8.1
+    # julia --project -e "using Pkg; Pkg.instantiate()"
+    julia --project scripts/main.jl $type --task=task/$(d)d$(L)L.toml
+    """
+end
+
+@cast function csm()
+    slurm_dir = joinpath(dirname(@__FILE__), "slurm")
+    ispath(slurm_dir) || mkpath(slurm_dir)
+
+    for d in 3:4, L in 4:2:16
+        open("scripts/slurm/csm_$(d)d$(L)L.sh", "w") do io
+            print(io, template("csm", d, L, 32, 16))
+        end
+        run(`sbatch scripts/slurm/csm_$(d)d$(L)L.sh`)
+    end
+end
+
+@cast function annealing()
+    slurm_dir = joinpath(dirname(@__FILE__), "slurm")
+    ispath(slurm_dir) || mkpath(slurm_dir)
+
+    for d in 3:4, L in 4:2:16
+        open("scripts/slurm/annealing_$(d)d$(L)L.sh", "w") do io
+            print(io, template("annealing", d, L, 1, 4))
+        end
+        run(`sbatch scripts/slurm/annealing_$(d)d$(L)L.sh`)
+    end
+end
+
+@cast function resample()
+    slurm_dir = joinpath(dirname(@__FILE__), "slurm")
+    ispath(slurm_dir) || mkpath(slurm_dir)
+
+    for d in 3:4, L in 4:2:16
+        open("scripts/slurm/resample_$(d)d$(L)L.sh", "w") do io
+            print(io, template("resample", d, L, 1, 4))
+        end
+        run(`sbatch scripts/slurm/resample_$(d)d$(L)L.sh`)
+    end
+end
+
+@main
--- src/cli.jl
+++ src/cli.jl
@@ -1,35 +1,55 @@
 module CLI
 
 using Comonicon
+using Serialization
 using Configurations
 using TerminalLoggers: TerminalLogger
 using Logging: with_logger
 using ..SimplexThreeGT:
+    CubicSpinMap,
     SimplexThreeGT,
     ChainTaskInfo, TaskInfo,
     ShapeInfo, SamplingInfo, Schedule,
     shape_dir, task_dir,
     SimplexMCMC, annealing!
 
-@cast function annealing(;task::ChainTaskInfo)
-    mcmc = SimplexMCMC(task)
-    log_file = task_dir(task, "annealing.log")
+
+function with_task_log(f, task::ChainTaskInfo, name::String)
+    log_file = task_dir(task, "$name.log")
     open(log_file, "w") do io
-        with_logger(TerminalLogger(io; always_flush=true)) do
-            @info "annealing starts" task
-            annealing!(mcmc, task)
-        end
+        with_logger(f, TerminalLogger(io; always_flush=true))
+    end
+    return
+end
+
+@cast function annealing(;task::ChainTaskInfo)
+    with_task_log(task, "annealing") do
+        @info "annealing starts" task
+        mcmc = SimplexMCMC(task)
+        annealing!(mcmc, task)
     end
     return
 end
 
 @cast function resample(;task::ChainTaskInfo)
-    log_file = task_dir(task, "annealing.log")
-    open(log_file, "w") do io
-        with_logger(TerminalLogger(io; always_flush=true)) do
-            @info "resample starts" task
-            SimplexThreeGT.resample(task)
-        end
+    with_task_log(task, "resample") do
+        @info "resample starts" task
+        SimplexThreeGT.resample(task)
+    end
+    return
+end
+
+@cast function csm(;task::ChainTaskInfo)
+    csm_cache = shape_dir(task.shape, "csm.jls")
+    if isfile(csm_cache)
+        @info "csm file exists" task
+        return
+    end
+
+    with_task_log(task, "csm") do
+        @info "csm generating starts" task
+        csm = CubicSpinMap(task.shape)
+        serialize(csm_cache, csm)
     end
     return
 end
