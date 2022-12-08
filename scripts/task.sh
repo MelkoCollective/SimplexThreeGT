a065254083c85b94923759b337339481c6e3d88e
Roger-luo <rogerluo.rl18@gmail.com>
2022-12-07 23:19:01 -0500

    update slurm

--- scripts/task.sh
+++ scripts/task.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#SBATCH --account=kfm-471-aa
+#SBATCH --account=rrg-rgmelko-ab
 #SBATCH --time=48:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --array=4-16:2
+#SBATCH --mem=4096M
+#SBATCH -o logs/%j.out
+#SBATCH -e logs/%j.err
 module load julia/1.8.1
-julia --project scripts/main.jl annealing --task=task/3d${SLURM_ARRAY_JOB_ID}L.toml
+# julia --project -e "using Pkg; Pkg.instantiate()"
+julia --project scripts/main.jl annealing --task=task/3d16L.toml
