f73e6887e51fdd4be88d43d630d94dca5d952d81
Roger-luo <rogerluo.rl18@gmail.com>
2022-12-12 20:33:35 -0500

    fix slurm path

--- scripts/submit.jl
+++ scripts/submit.jl
@@ -71,7 +71,7 @@ function template(type, d, L, nthreads::Int, mem::Int)
     #SBATCH -e logs/%j.err
     module load julia/1.8.1
     # julia --project -e "using Pkg; Pkg.instantiate()"
-    julia --project --threads=$nthreads scripts/main.jl $type --task=task/$type-$(d)d$(L)L.toml
+    julia --project --threads=$nthreads scripts/main.jl $type --task=scripts/task/$type-$(d)d$(L)L.toml
     """
 end
 
